0c880cc17d97c2ae841334c5dfd51f8d985fcb1a
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-26 12:03:00 +0100

    Add importer thread for data collection from lidar

--- include/hokuyo_lidar_importer.h
+++ include/hokuyo_lidar_importer.h
@@ -1,6 +1,8 @@
 #ifndef HOKUYO_LIDAR_IMPORTER_H
 #define HOKUYO_LIDAR_IMPORTER_H
 
+#include <atomic>
+#include <thread>
 #include <vector>
 
 #include <cpp/Urg_driver.h>
@@ -16,15 +18,28 @@ class HokuyoLidarImporter : public lms::Module {
     bool cycle();
 
    private:
-    void printLidarData(const std::vector<long>& data);
-
-    lms::WriteDataChannel<lms::math::PointCloud2f> data;
+    void startImporterThread();
+    void prepRawData();
 
     qrk::Urg_driver m_lidar;
+    std::vector<long> m_rawDataPoints;
+    bool m_newImport;
 
+    ////////////////////////////// Data Channels ///////////////////////////////
+    lms::WriteDataChannel<lms::math::PointCloud2f> data;
+    lms::WriteDataChannel<bool> newData;
+
+    /////////////////////////////// Config Values //////////////////////////////
+    bool m_configsChanged;
     lms::math::vertex2f m_offsetFromOrigin;
     double m_startAtDeg;
     double m_stopAtDeg;
+
+    /////////////////////////////// Thread & Mutex /////////////////////////////
+    std::mutex m_dataMutex;
+    std::mutex m_lidarMutex;
+    std::thread m_importer;
+    std::atomic_bool a_importerRunning;
 };
 
 #endif  // HOKUYO_LIDAR_IMPORTER_H
--- src/hokuyo_lidar_importer.cpp
+++ src/hokuyo_lidar_importer.cpp
@@ -1,6 +1,7 @@
 #include "hokuyo_lidar_importer.h"
 
 #include <math.h>
+#include <utility>
 
 namespace {
 const char* kDeviceFile = "/dev/ttyACM0";
@@ -9,7 +10,10 @@ const long kBaudrate = 115200;
 
 bool HokuyoLidarImporter::initialize() {
     data = writeChannel<lms::math::PointCloud2f>("HOKUYO_LIDAR_DATA");
+    newData = writeChannel<bool>("NEW_DATA");
+
     configsChanged();
+    m_configsChanged = false;
 
     if (!m_lidar.open(kDeviceFile, kBaudrate, qrk::Urg_driver::Serial)) {
         logger.error() << "Failed opening Hokuyo Lidar at " << kDeviceFile
@@ -19,13 +23,15 @@ bool HokuyoLidarImporter::initialize() {
     m_lidar.set_scanning_parameter(m_lidar.deg2step(m_startAtDeg),
                                    m_lidar.deg2step(m_stopAtDeg), 0);
     m_lidar.start_measurement();
-
+    startImporterThread();
     return true;
 }
 
 bool HokuyoLidarImporter::deinitialize() {
+    a_importerRunning = false;
+    m_importer.join();
     m_lidar.stop_measurement();
-
+    m_lidarMutex.unlock();
     return true;
 }
 
@@ -34,36 +40,60 @@ void HokuyoLidarImporter::configsChanged() {
     m_offsetFromOrigin.y = config().get<float>("yOffsetFromOrigin", 0);
     m_startAtDeg = config().get<double>("startAtDeg", -90);
     m_stopAtDeg = config().get<double>("stopAtDeg", 90);
+    m_configsChanged = true;
 }
 
 bool HokuyoLidarImporter::cycle() {
-    std::vector<long> rawDataPoints;
-    if (!m_lidar.get_distance(rawDataPoints)) {
-        logger.error() << "Failed getting distance from Hokuyo Lidar error: "
-                       << m_lidar.what();
-        return false;
+    std::lock_guard<std::mutex> lock(m_dataMutex);
+    if (!m_newImport) {
+        *newData = false;
+        return true;
     }
+    m_newImport = false;
+    *newData = true;
 
+    prepRawData();
+
+    if (m_configsChanged) {
+        m_lidar.set_scanning_parameter(m_lidar.deg2step(m_startAtDeg),
+                                       m_lidar.deg2step(m_stopAtDeg), 0);
+    }
+    m_lidarMutex.unlock();
+    return true;
+}
+
+void HokuyoLidarImporter::startImporterThread() {
+    m_newImport = false;
+    a_importerRunning = true;
+    m_importer = std::thread([this]() {
+        while (a_importerRunning.load()) {
+            std::vector<long> measurement;
+            m_lidarMutex.lock();
+            if (!m_lidar.get_distance(measurement)) {
+                logger.error()
+                    << "Failed getting distance from Hokuyo Lidar error: "
+                    << m_lidar.what();
+                m_lidarMutex.unlock();
+                continue;
+            }
+            {
+                std::lock_guard<std::mutex> lock(m_dataMutex);
+                m_rawDataPoints = std::move(measurement);
+                m_newImport = true;
+            }
+        }
+    });
+}
+
+void HokuyoLidarImporter::prepRawData() {
     data->points().clear();
-    data->points().resize(rawDataPoints.size());
-    for (std::vector<long>::size_type i = 0; i < rawDataPoints.size(); i++) {
-        long l = rawDataPoints[i];
+    data->points().resize(m_rawDataPoints.size());
+    for (std::vector<long>::size_type i = 0; i < m_rawDataPoints.size(); i++) {
+        long l = m_rawDataPoints[i];
         double radian = m_lidar.index2rad(i);
         long x = l * cos(radian);
         long y = l * sin(radian);
         data->points().push_back(lms::math::vertex2f(x, y) / 1000 +
                                  m_offsetFromOrigin);
-    }
-
-    return true;
-}
-
-void HokuyoLidarImporter::printLidarData(const std::vector<long>& data) {
-    for (std::vector<long>::size_type i = 0; i < data.size(); i++) {
-        long l = data[i];
-        double radian = m_lidar.index2rad(i);
-        long x = l * cos(radian);
-        long y = l * sin(radian);
-        logger.info("LidarData") << "(" << x << ", " << y << ")[mm]";
     }
 }
